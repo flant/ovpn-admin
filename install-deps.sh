c27229e920495f42ecf3275350597efefdb18c46
Ilya Sosnovsky <ilya.sosnovsky@flant.com>
2021-10-15 08:05:30 +0300

    build changes

--- install-deps.sh
+++ install-deps.sh
@@ -3,9 +3,9 @@
 
 apt-get update
 apt-get install -y curl
-apt-get install -y gcc-multilib  libc6-dev-i386 linux-libc-dev:i386
+apt-get install -y gcc-multilib libc6 libc6-dev libc6-dev-i386
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
 
 PATH=$PATH:~/go/bin
@@ -15,5 +15,3 @@ go get -u github.com/gobuffalo/packr/v2/packr2
 cd frontend && npm install && npm run build && cd ..
 
 packr2
-
-export CGO_ENABLED=1
