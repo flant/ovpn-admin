d3b5a77efb263c149df515881563538d2873df23
Ilya Sosnovsky <ilya.sosnovsky@flant.com>
2021-11-29 13:40:05 +0300

    fixes for build scripts

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ cd frontend && npm install && npm run build && cd ..
 
 packr2
 
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "-linkmode external -extldflags -static -s -w" @
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "-linkmode external -extldflags -static -s -w" $@
 
 packr2 clean
