83237f822164cb1c09bc2a3821e69770499cfe34
Sprait <spraitt@gmail.com>
2023-11-22 15:18:57 +0000

    del old easyrsa

--- setup/configure.sh
+++ setup/configure.sh
@@ -19,17 +19,7 @@ else
     do
       sleep 5
     done
-  else
-    echo "Generating new certs"
-    easyrsa init-pki
-    cp -R /usr/share/easy-rsa/* $EASY_RSA_LOC/pki
-    echo "ca" | easyrsa build-ca nopass
-    easyrsa build-server-full server nopass
-    easyrsa gen-dh
-    openvpn --genkey --secret ./pki/ta.key
-  fi
 fi
-easyrsa gen-crl
 
 iptables -t nat -D POSTROUTING -s ${OVPN_SRV_NET}/${OVPN_SRV_MASK} ! -d ${OVPN_SRV_NET}/${OVPN_SRV_MASK} -j MASQUERADE || true
 iptables -t nat -A POSTROUTING -s ${OVPN_SRV_NET}/${OVPN_SRV_MASK} ! -d ${OVPN_SRV_NET}/${OVPN_SRV_MASK} -j MASQUERADE
