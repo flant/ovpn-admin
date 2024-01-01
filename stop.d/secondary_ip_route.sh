fa11b80176e8c841d10aa20b4c7473a1fa1c3869
Farid Musa <mmtechslv@gmail.com>
2024-01-01 22:29:54 +0400

    fix

--- stop.d/secondary_ip_route.sh
+++ stop.d/secondary_ip_route.sh
@@ -2,4 +2,4 @@
 set -ex
 
 default_iface=$(ip route | grep default | awk '{print $5}')
-iptables -t nat -I POSTROUTING -s "$OVPN_NET_SUBNET" -o "$default_iface" -j SNAT --to-source "$OVPN_PUBLIC_IP"
+iptables -t nat -D POSTROUTING -s "$OVPN_NET_SUBNET" -o "$default_iface" -j SNAT --to-source "$OVPN_PUBLIC_IP"
