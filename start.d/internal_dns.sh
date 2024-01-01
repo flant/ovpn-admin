71393a503e8388a4f9d71634ee7bc788b859d1be
Farid Musa <mmtechslv@gmail.com>
2024-01-01 21:58:53 +0400

    fix

--- start.d/internal_dns.sh
+++ start.d/internal_dns.sh
@@ -4,5 +4,5 @@ set -exo pipefail
 dns_container_id="$(docker ps | grep "$OVPN_DNS_CONTAINER_NAME_PATTERN" | awk '{ print $1 }' 2> /dev/null)"
 if [ "$dns_container_id" != "" ]; then
   echo "Connecting network 'vpnet' to $dns_container_id"
-  docker network connect vpnet "$dns_container_id"
+  docker network connect ${OVPN_COMPOSE_NAME}_vpnet "$dns_container_id"
 fi
