9d21df39a46e34172b885f1762efc88fe20c50a2
Farid Musa <mmtechslv@gmail.com>
2024-01-01 21:59:29 +0400

    fix

--- stop.d/internal_dns.sh
+++ stop.d/internal_dns.sh
@@ -4,5 +4,5 @@ set -exo pipefail
 dns_container_id="$(docker ps | grep "$OVPN_DNS_CONTAINER_NAME_PATTERN" | awk '{ print $1 }' 2> /dev/null)"
 if [ "$dns_container_id" != "" ]; then
   echo "Disconnecting network 'vpnet' from $dns_container_id"
-  docker network disconnect ${OVPN_COMPOSE_NAME}_vpnet "$dns_container_id"
+  docker network disconnect ${OVPN_COMPOSE_NAME}_ovpn-net "$dns_container_id"
 fi
