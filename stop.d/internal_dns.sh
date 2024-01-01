bb6e87d645d20e0faf85e4605f7a4378f4f11b1e
Farid Musa <mmtechslv@gmail.com>
2024-01-01 22:29:04 +0400

    fixes

--- stop.d/internal_dns.sh
+++ stop.d/internal_dns.sh
@@ -6,9 +6,9 @@ if [ "$dns_container_id" != "" ]; then
   echo "Found running DNS container $dns_container_id"
   existing_network_name="$(docker inspect "$dns_container_id" | jq -r '.[0].NetworkSettings.Networks | keys | .[0] ')"
   echo "Found network '$existing_network_name' connected to '$dns_container_id'"
-  vpn_container_id="$(docker ps | grep "${OVPN_COMPOSE_NAME}" | grep "openvpn" | awk '{ print $1 }' 2> /dev/null)"
+  vpn_container_id="$(docker ps | grep "${OVPN_COMPOSE_NAME}" | grep "openvpn" | awk '{ print $1 }' 2> /dev/null || true)"
   echo "Disconnecting network '$existing_network_name' to $vpn_container_id"
-  docker network disconnect "$existing_network_name" "$vpn_container_id"
+  docker network disconnect "$existing_network_name" "$vpn_container_id" || true
   echo "Disconnecting network '${OVPN_COMPOSE_NAME}_ovpn-net' to $dns_container_id"
-  docker network disconnect "${OVPN_COMPOSE_NAME}_ovpn-net" "$dns_container_id"
+  docker network disconnect "${OVPN_COMPOSE_NAME}_ovpn-net" "$dns_container_id" || true
 fi
